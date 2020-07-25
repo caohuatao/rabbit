31a93b5c7905a52da7a6166ae57f9057e8f7fd37
caohuatao <351995664@qq.com>
2020-07-26 00:38:48 +0800

    change home  router

---
+++ src/pages/home/views/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-container>
+    <el-header height="40px">
+      <el-col
+        class="header__title"
+        :span="16">
+        <i class="el-icon-document"></i>
+        <h3>111</h3>
+      </el-col>
+      <el-col :span="8">
+        <el-button
+          size="mini"
+          type="danger"
+          icon="el-icon-delete">
+          {{$t('remove')}}
+        </el-button>
+        <el-button
+          size="mini"
+          type="primary"
+          icon="el-icon-upload">
+          {{$t('publish')}}
+        </el-button>
+      </el-col>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <header></header>
+        <ul>
+          <li></li>
+        </ul>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  export default {
+  }
+</script>
